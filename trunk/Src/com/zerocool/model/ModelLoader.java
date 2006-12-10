c63f09109c7afc22a1640485fb82cc9159089da6
rokkun <rokkun@04aa7f75-fc23-0410-a342-61c1dcb5cb08>
2006-12-10 07:29:55 +0000

    Correct Model Loader

---
+++ trunk/Src/com/zerocool/model/ModelLoader.java
@@ -0,0 +1,50 @@
+package com.zerocool.model;
+import java.net.URL;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
+
+import com.jme.math.Matrix3f;
+import com.jme.math.Quaternion;
+import com.jme.scene.Node;
+import com.jme.util.export.binary.BinaryImporter;
+import com.jmex.model.XMLparser.Converters.MaxToJme;
+public class ModelLoader {
+	MaxToJme maxtojme = new MaxToJme();
+	ByteArrayOutputStream bytearrayoutputstream = new ByteArrayOutputStream();
+	Node node = new Node();
+	
+	public Node loadModel(String filename){
+		
+	InputStream is = null;
+	try {
+		is = new FileInputStream(new File(filename));
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+
+	try {
+	 
+	    // Converts the file into a jme usable file
+	    maxtojme.convert(is, bytearrayoutputstream);
+	 
+	    // Used to convert the jme usable file to a TriMesh
+	    BinaryImporter binaryImporter = new BinaryImporter(); 
+	    ByteArrayInputStream in=new ByteArrayInputStream(bytearrayoutputstream.toByteArray());
+	 
+	    //importer returns a Loadable, cast to Node
+	    node = (Node)binaryImporter.load(in); 
+	    
+	} catch(Exception err)  {
+	    System.out.println("Error loading 3DS model: "+err);
+	}
+	node.setLocalScale(.1f);
+	node.setLocalRotation(new Quaternion(new float[]{(float)-Math.PI / 2f,0,0}));
+	return node;
+}
+}
--- trunk/Src/com/zerocool/scene/RenderableObject.java
+++ trunk/Src/com/zerocool/scene/RenderableObject.java
@@ -4,17 +4,11 @@ import com.jme.math.Vector3f;
 import com.jme.scene.Node;
 import com.jme.scene.Spatial;
 import com.jme.scene.shape.Box;
-import java.net.URL;
-import java.io.InputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ByteArrayInputStream;
-import com.jme.util.export.binary.BinaryImporter;
-import com.jmex.model.XMLparser.Converters.MaxToJme;
+
 
 public class RenderableObject extends UpdatableNode {
 	private Spatial object;
-	MaxToJme maxtojme = new MaxToJme();
-	ByteArrayOutputStream bytearrayoutputstream = new ByteArrayOutputStream();
+
 	Node node = new Node();
 	
 	public RenderableObject(){
@@ -34,31 +28,7 @@ public class RenderableObject extends UpdatableNode {
 		attachChild(object);
 	}
 	
-	public void loadModel(String filename){
-		URL url = RenderableObject.class.getClassLoader().getResource(filename); 
-		
-		InputStream is=null;
-		try {
-		    is=url.openStream();
-		} catch(Exception err)  {
-		    System.out.println("Could not open Model file: "+err);
-		}
-		try {
-		 
-		    // Converts the file into a jme usable file
-		    maxtojme.convert(is, bytearrayoutputstream);
-		 
-		    // Used to convert the jme usable file to a TriMesh
-		    BinaryImporter binaryImporter = new BinaryImporter(); 
-		    ByteArrayInputStream in=new ByteArrayInputStream(bytearrayoutputstream.toByteArray());
-		 
-		    //importer returns a Loadable, cast to Node
-		    node = (Node)binaryImporter.load(in); 
-		    this.attachChild(node);
-		} catch(Exception err)  {
-		    System.out.println("Error loading md3 model:"+err);
-		}
-	}
+
 	
 	public Spatial getRenderObject(){
 		return object;
