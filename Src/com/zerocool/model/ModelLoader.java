d499a83c834a504c27ab8f5e916417e137d3066a
rokkun <rokkun@04aa7f75-fc23-0410-a342-61c1dcb5cb08>
2006-12-11 01:55:27 +0000

    Fixed (Most) problems with model loading

--- Src/com/zerocool/model/ModelLoader.java
+++ Src/com/zerocool/model/ModelLoader.java
@@ -10,6 +10,7 @@ import java.io.ByteArrayInputStream;
 import com.jme.math.Matrix3f;
 import com.jme.math.Quaternion;
 import com.jme.scene.Node;
+import com.jme.util.TextureKey;
 import com.jme.util.export.binary.BinaryImporter;
 import com.jmex.model.XMLparser.Converters.MaxToJme;
 public class ModelLoader {
@@ -32,18 +33,19 @@ public class ModelLoader {
 	 
 	    // Converts the file into a jme usable file
 	    maxtojme.convert(is, bytearrayoutputstream);
-	 
+	    
 	    // Used to convert the jme usable file to a TriMesh
 	    BinaryImporter binaryImporter = new BinaryImporter(); 
 	    ByteArrayInputStream in=new ByteArrayInputStream(bytearrayoutputstream.toByteArray());
-	 
+	    TextureKey.setOverridingLocation(new URL("file:/C:\\zerocool\\Zero Cool\\Data\\models\\H1_TEX.BMP"));
+	    
 	    //importer returns a Loadable, cast to Node
 	    node = (Node)binaryImporter.load(in); 
 	    
 	} catch(Exception err)  {
 	    System.out.println("Error loading 3DS model: "+err);
 	}
-	node.setLocalScale(.1f);
+	node.setLocalScale(.05f);
 	node.setLocalRotation(new Quaternion(new float[]{(float)-Math.PI / 2f,0,0}));
 	return node;
 }
