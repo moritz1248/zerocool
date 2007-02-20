45fa9f5068c405b796dcd225a92f3fd5ea18b1a0
cgibsonPZC <cgibsonPZC@04aa7f75-fc23-0410-a342-61c1dcb5cb08>
2007-02-20 19:36:25 +0000

    Polygon structure

---
+++ Src/com/jfysics/physics/bodies/polygon/RigidPolygon.java
@@ -0,0 +1,9 @@
+package com.jfysics.physics.bodies.polygon;
+
+import java.util.ArrayList;
+
+import com.jfysics.math.vector.Vector2d;
+
+public class RigidPolygon {
+	private ArrayList<Vector2d> Vectors;
+}
