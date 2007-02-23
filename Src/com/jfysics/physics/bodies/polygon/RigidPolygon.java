37b9113bdfcaf5bd86652b0a8cb2ac3bbf9a5f30
cgibsonPZC <cgibsonPZC@04aa7f75-fc23-0410-a342-61c1dcb5cb08>
2007-02-23 01:40:27 +0000

    Added BoundingBox class to handle bounding boxes for all objects. Built up polygon class to include ability to generate bounding boxes Updated regression test.

--- Src/com/jfysics/physics/bodies/polygon/RigidPolygon.java
+++ Src/com/jfysics/physics/bodies/polygon/RigidPolygon.java
@@ -1,9 +1,41 @@
 package com.jfysics.physics.bodies.polygon;
 
+import java.awt.Rectangle;
 import java.util.ArrayList;
 
 import com.jfysics.math.vector.Vector2d;
+import com.jfysics.physics.bodies.BoundingBox;
 
 public class RigidPolygon {
-	private ArrayList<Vector2d> Vectors;
+	private ArrayList<Vector2d> vectors;
+	private BoundingBox boundingBox;
+	public RigidPolygon(ArrayList<Vector2d> vecList){
+		vectors = vecList;
+	}
+	public BoundingBox getBounds()
+	{
+		if(boundingBox != null)
+		{
+			return boundingBox;
+		}else{
+			generateBoundingBox();
+			return boundingBox;
+		}
+	}
+	private void generateBoundingBox()
+	{
+		Vector2d min = vectors.get(0), max = vectors.get(0);
+		for(Vector2d v : vectors)
+		{
+			if(v.getX() < min.getX())
+				min.setX(v.getX());
+			if(v.getY() < min.getY())
+				min.setY(v.getY());
+			if(v.getX() > max.getX())
+				max.setX(v.getX());
+			if(v.getY() > max.getY())
+				max.setY(v.getY());
+		}
+		boundingBox = new BoundingBox(min, max);
+	}
 }
