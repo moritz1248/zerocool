8ea8da24571aeceb154fca1a67bded139eae9b3d
cgibsonPZC <cgibsonPZC@04aa7f75-fc23-0410-a342-61c1dcb5cb08>
2007-04-10 08:53:03 +0000

    Task: #15 Implemented level compiler and added toString method for game object

---
+++ trunk/Src/com/zerocool/level/LevelCompilerTest.java
@@ -0,0 +1,9 @@
+package com.zerocool.level;
+
+public class LevelCompilerTest {
+	public static void main(String ... args)
+	{
+		LevelCompiler compiler = new LevelCompiler();
+		compiler.load("Data\\levels\\test.zcl");
+	}
+}
