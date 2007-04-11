7a016296f01233f828c1d6840a0bfbe7e2f4b003
cgibsonPZC <cgibsonPZC@04aa7f75-fc23-0410-a342-61c1dcb5cb08>
2007-04-11 06:15:38 +0000

    Task: #15

--- trunk/Src/com/zerocool/model/ModelLoader.java
+++ trunk/Src/com/zerocool/model/ModelLoader.java
@@ -1,4 +1,5 @@
 package com.zerocool.model;
+
 import java.net.URL;
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,38 +14,43 @@ import com.jme.scene.Node;
 import com.jme.util.TextureKey;
 import com.jme.util.export.binary.BinaryImporter;
 import com.jmex.model.XMLparser.Converters.MaxToJme;
+
 public class ModelLoader {
 	MaxToJme maxtojme = new MaxToJme();
+
 	ByteArrayOutputStream bytearrayoutputstream = new ByteArrayOutputStream();
+
 	Node node = new Node();
-	
-	public Node loadModel(String filename){
-	InputStream is = null;
-	try {
-		is = new FileInputStream(new File(filename));
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
 
+	public Node loadModel(String filename) {
+		InputStream is = null;
+		try {
+			is = new FileInputStream(new File(filename));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-	try {
-	 
-	    // Converts the file into a jme usable file
-	    maxtojme.convert(is, bytearrayoutputstream);
-	    
-	    // Used to convert the jme usable file to a TriMesh
-	    BinaryImporter binaryImporter = new BinaryImporter(); 
-	    ByteArrayInputStream in=new ByteArrayInputStream(bytearrayoutputstream.toByteArray());
-	    TextureKey.setOverridingLocation(new URL("file:/" + System.getProperty("user.dir") + "\\Data\\textures\\"));
-	    //importer returns a Loadable, cast to Node
-	    node = (Node)binaryImporter.load(in); 
-	    
-	} catch(Exception err)  {
-	    System.out.println("Error loading 3DS model: "+err);
+		try {
+
+			// Converts the file into a jme usable file
+			maxtojme.convert(is, bytearrayoutputstream);
+
+			// Used to convert the jme usable file to a TriMesh
+			BinaryImporter binaryImporter = new BinaryImporter();
+			ByteArrayInputStream in = new ByteArrayInputStream(
+					bytearrayoutputstream.toByteArray());
+			TextureKey.setOverridingLocation(new URL("file:/"
+					+ System.getProperty("user.dir") + "\\Data\\textures\\"));
+			// importer returns a Loadable, cast to Node
+			node = (Node) binaryImporter.load(in);
+
+		} catch (Exception err) {
+			System.out.println("Error loading 3DS model: " + err);
+		}
+		node.setLocalScale(.05f);
+		node.setLocalRotation(new Quaternion(new float[] {
+				(float) -Math.PI / 2f, 0, 0 }));
+		return node;
 	}
-	node.setLocalScale(.05f);
-	node.setLocalRotation(new Quaternion(new float[]{(float)-Math.PI / 2f,0,0}));
-	return node;
-}
 }
